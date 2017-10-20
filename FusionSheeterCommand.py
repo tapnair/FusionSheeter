4a939b38f081a2c3caa1b86bd7129d55e146d9c6
Patrick Rainsberry <prrainsberry@yahoo.com>
2017-10-20 16:59:49 -0700

    Fixed Credentials issue

--- FusionSheeterCommand.py
+++ FusionSheeterCommand.py
@@ -83,7 +83,7 @@ def get_credentials():
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        credentials = tools.run(flow, store)
+        credentials = tools.run_flow(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
 
